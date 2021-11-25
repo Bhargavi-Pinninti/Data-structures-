d6cbbc0c99ababcedb14fd5c6e4ea5437781ef91
Bhargavi-Pinninti <85938504+Bhargavi-Pinninti@users.noreply.github.com>
2021-11-25 15:53:03 +0530

    Update Main.java

--- Searching/Main.java
+++ Searching/Main.java
@@ -9,30 +9,30 @@ import  java.util.Scanner;
 public class Linear {
   public static void main(String args[])  
   {  
-    int c, n,search, array[];  
+    int i, n, k, arr[];  
    
-    Scanner in = new Scanner(System.in);  
+    Scanner input = new Scanner(System.in);  
     System.out.println("Enter number of elements");  
-    n = in.nextInt();   
-    array = new int[n];  
+    n = input.nextInt();   
+    arr = new int[n];  
    
     System.out.println("Enter those " + n + " elements");  
    
-    for (c = 0; c < n; c++)  
-      array[c] = in.nextInt();  
+    for (i = 0; i < n; i++)  
+      arr[i] = input.nextInt();  
    
     System.out.println("Enter value to find");  
-    search = in.nextInt();  
+    k = input.nextInt();  
    
-    for (c = 0; c < n; c++) 
+    for (i = 0; i < n; i++) 
     {  
-      if (array[c] == search)      
+      if (arr[i] == k)      
       {  
-         System.out.println(search + " is present at location " + (c + 1) + ".");  
+         System.out.println(k + " is present at location " + (i + 1) + ".");  
           break;  
       }  
    }  
-   if (c == n)  
-      System.out.println(search + " isn't present in array.");  
+   if (k == n)  
+      System.out.println(k + " isn't present in array.");  
   }  
 } 
